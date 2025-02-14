cd48e2437587c0c98b6d51998e30b7bc02bbdc08
dpesce <dom.pesce@gmail.com>
2025-02-13 22:12:22 -0500

    add line plots and associated pages

--- make_plots.py
+++ make_plots.py
@@ -6,11 +6,12 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
+from astropy.io import fits
 
 ##################################################
 # inputs
 
-make_spectrum_figure = True
+make_spectrum_figure = False
 make_SED_figure = False
 
 ##################################################
@@ -53,6 +54,7 @@ speclines = {36.94: 'OH [?]',
              112.98: 'CN',
              113.33: 'CN',
              115.10: 'CO',
+             135.10: 'H<sub>2</sub>CS [?]',
              140.64: 'H<sub>2</sub>CO',
              145.39: 'H<sub>2</sub>CO [?]',
              146.76: 'CS',
@@ -301,10 +303,11 @@ if make_spectrum_figure:
 
     # output interactive html plot
     fig.write_html('plots/spectrum.html')
-
 ##################################################
-# continuum info
+##################################################
+# generate SED plot
 
+# continuum info
 freqs = [39.40633132032,46.05395407602,97.89582649825999,107.2456922428,137.6965151531]
 bws = [8.78122053592,7.657156181928,30.72496072362,17.28863737626,26.320344557520002]
 bmajs = [9.5243530273452,3.537628173828,3.03612208366392,1.71604394912736,2.1782915592192]
@@ -424,4 +427,351 @@ if make_SED_figure:
     fig.write_html('plots/SED.html')
 
 ##################################################
+# line cubes
+##################################################
+
+from scipy.optimize import curve_fit
+import matplotlib
+import matplotlib.pyplot as plt
+import ehtplot
+
+c = 299792.458
+vrec = 434.0
+SNR_cut = 5.0
+
+write_tables = {'OH_36.94GHz': False,
+                'CS_48.92GHz': False,
+                'H13CN_86.22GHz': False,
+                'H13CO_86.63GHz': False,
+                'CCH_87.19GHz': False,
+                'CCH_87.28GHz': False,
+                'HCN_88.50GHz': False,
+                'HCO_89.06GHz': False,
+                'HNC_90.53GHz': False,
+                'CS_97.84GHz': False,
+                'C18O_109.62GHz': False,
+                '13CO_110.04GHz': False,
+                'CN_112.98GHz': False,
+                'CN_113.33GHz': False,
+                'CO_115.10GHz': False,
+                'H2CS_135.10GHz': False,
+                'H2CO_140.64GHz': False,
+                'H2CO_145.39GHz': False,
+                'CS_146.76GHz': False,
+                'H2CO_150.28GHz': False
+                }
+
+make_plots = {'OH_36.94GHz': False,
+              'CS_48.92GHz': False,
+              'H13CN_86.22GHz': False,
+              'H13CO_86.63GHz': False,
+              'CCH_87.19GHz': False,
+              'CCH_87.28GHz': False,
+              'HCN_88.50GHz': False,
+              'HCO_89.06GHz': False,
+              'HNC_90.53GHz': False,
+              'CS_97.84GHz': False,
+              'C18O_109.62GHz': False,
+              '13CO_110.04GHz': False,
+              'CN_112.98GHz': False,
+              'CN_113.33GHz': False,
+              'CO_115.10GHz': False,
+              'H2CS_135.10GHz': False,
+              'H2CO_140.64GHz': False,
+              'H2CO_145.39GHz': False,
+              'CS_146.76GHz': False,
+              'H2CO_150.28GHz': False
+              }
+
+nu0_dict = {'OH_36.94GHz': 36.9944102,
+            'CS_48.92GHz': 48.9909549,
+            'H13CN_86.22GHz': 86.3399214,
+            'H13CO_86.63GHz': 86.7542884,
+            'CCH_87.19GHz': 87.316925,
+            'CCH_87.28GHz': 87.407165,
+            'HCN_88.50GHz': 88.6316023,
+            'HCO_89.06GHz': 89.1885247,
+            'HNC_90.53GHz': 90.663568,
+            'CS_97.84GHz': 97.9809533,
+            'C18O_109.62GHz': 109.7821734,
+            '13CO_110.04GHz': 110.2013543,
+            'CN_112.98GHz': 113.16879389,
+            'CN_113.33GHz': 113.49438589,
+            'CO_115.10GHz': 115.2712018,
+            'H2CS_135.10GHz': 135.297811,
+            'H2CO_140.64GHz': 140.8395197167,
+            'H2CO_145.39GHz': 145.602949,
+            'CS_146.76GHz': 146.9690287,
+            'H2CO_150.28GHz': 150.498334
+            }
+
+rms_ind_start_dict = {'OH_36.94GHz': 120,
+                      'CS_48.92GHz': 200,
+                      'H13CN_86.22GHz': 100,
+                      'H13CO_86.63GHz': 100,
+                      'CCH_87.19GHz': 100,
+                      'CCH_87.28GHz': 100,
+                      'HCN_88.50GHz': 100,
+                      'HCO_89.06GHz': 100,
+                      'HNC_90.53GHz': 100,
+                      'CS_97.84GHz': 100,
+                      'C18O_109.62GHz': 100,
+                      '13CO_110.04GHz': 100,
+                      'CN_112.98GHz': 250,
+                      'CN_113.33GHz': 250,
+                      'CO_115.10GHz': 128,
+                      'H2CS_135.10GHz': 100,
+                      'H2CO_140.64GHz': 100,
+                      'H2CO_145.39GHz': 100,
+                      'CS_146.76GHz': 100,
+                      'H2CO_150.28GHz': 100
+                      }
+
+xmax_dict = {'OH_36.94GHz': 27,
+             'CS_48.92GHz': 20,
+             'H13CN_86.22GHz': 27,
+             'H13CO_86.63GHz': 27,
+             'CCH_87.19GHz': 20,
+             'CCH_87.28GHz': 20,
+             'HCN_88.50GHz': 27,
+             'HCO_89.06GHz': 27,
+             'HNC_90.53GHz': 27,
+             'CS_97.84GHz': 20,
+             'C18O_109.62GHz': 27,
+             '13CO_110.04GHz': 27,
+             'CN_112.98GHz': 20,
+             'CN_113.33GHz': 20,
+             'CO_115.10GHz': 27,
+             'H2CS_135.10GHz': 10,
+             'H2CO_140.64GHz': 12,
+             'H2CO_145.39GHz': 12,
+             'CS_146.76GHz': 20,
+             'H2CO_150.28GHz': 12
+             }
+
+def gaussian(x,A,s,v0,S0):
+    return S0 + (A*np.exp(-0.5*((x-v0)**2.0)/(s**2.0)))
+
+# def write_table(v,cube,tablename,rms_ind_start=200):
+def write_table(linehere,rms_ind_start=200,bounds_amp=None):
+
+    infile = './data/line_cubes/'+linehere+'.fits'
+    infile_freq = './data/line_cubes/'+linehere+'_freq.txt'
+    tablename = './data/line_cubes/gaussian_fits_'+linehere+'.txt'
+    nu0 = nu0_dict[linehere]
+
+    hdul = fits.open(infile)
+    cube = hdul[0].data
+    nu = np.loadtxt(infile_freq)
+    nu /= (1.0e9)
+    v = ((nu0 - nu)/nu)*c
+
+    # get axes in arcseconds
+    head = hdul[0].header
+    pixnumx = np.linspace(0.0,head['NAXIS1']-1,head['NAXIS1'])
+    pixnumy = np.linspace(0.0,head['NAXIS2']-1,head['NAXIS2'])
+    x = head['CDELT1']*(pixnumx - head['CRPIX1']) + head['CRVAL1']
+    y = head['CDELT2']*(pixnumy - head['CRPIX2']) + head['CRVAL2']
+    x0 = head['CDELT1']*(np.ceil(cube.shape[1]/2) - head['CRPIX1']) + head['CRVAL1']
+    y0 = head['CDELT2']*(np.ceil(cube.shape[2]/2) - head['CRPIX2']) + head['CRVAL2']
+    x_arcsec = (x - x0)*3600.0
+    y_arcsec = (y - y0)*3600.0
+
+    A = np.zeros_like(cube[0,:,:])
+    s = np.zeros_like(cube[0,:,:])
+    v0 = np.zeros_like(cube[0,:,:])
+    S0 = np.zeros_like(cube[0,:,:])
+    A_err = np.zeros_like(cube[0,:,:])
+    s_err = np.zeros_like(cube[0,:,:])
+    v0_err = np.zeros_like(cube[0,:,:])
+    S0_err = np.zeros_like(cube[0,:,:])
+
+    sigma = np.ones_like(v)*np.nanstd(cube[:,rms_ind_start:,rms_ind_start:])
+
+    for i in range(cube.shape[1]):
+        for j in range(cube.shape[2]):
+
+            print(i,j)
+
+            yhere = cube[:,i,j]
+
+            if (np.isfinite(yhere).sum() != 0) & (np.nanmax(yhere) > 0):
+
+                if bounds_amp is None:
+                    p0 = [np.nanmax(cube[:,i,j]),100.0,434.0,0.0]
+                    bounds = ([0.0,10.0,100.0,0.0],[2.0*np.nanmax(cube[:,i,j]),500.0,700.0,0.05])
+                else:
+                    p0 = [np.mean(bounds_amp),100.0,434.0,0.0]
+                    bounds = ([bounds_amp[0],10.0,100.0,0.0],[bounds_amp[1],500.0,700.0,0.05])
+                popt, pcov = curve_fit(gaussian,v,yhere,sigma=sigma,p0=p0,bounds=bounds,nan_policy='omit',maxfev=100000)
+
+                A[i,j], s[i,j], v0[i,j], S0[i,j] = popt
+                A_err[i,j] = np.sqrt(pcov[0,0])
+                s_err[i,j] = np.sqrt(pcov[1,1])
+                v0_err[i,j] = np.sqrt(pcov[2,2])
+                S0_err[i,j] = np.sqrt(pcov[3,3])
+
+            else:
+
+                A[i,j] = np.nan
+                s[i,j] = np.nan
+                v0[i,j] = np.nan
+                S0[i,j] = np.nan
+                A_err[i,j] = np.nan
+                s_err[i,j] = np.nan
+                v0_err[i,j] = np.nan
+                S0_err[i,j] = np.nan
+
+    with open(tablename,'w') as f:
+        for i in range(cube.shape[1]):
+            for j in range(cube.shape[2]):
+                strhere = ''
+                strhere += str(i).ljust(6)
+                strhere += str(j).ljust(6)
+                strhere += str(A[i,j]).ljust(24)
+                strhere += str(A_err[i,j]).ljust(24)
+                strhere += str(s[i,j]).ljust(24)
+                strhere += str(s_err[i,j]).ljust(24)
+                strhere += str(v0[i,j]).ljust(24)
+                strhere += str(v0_err[i,j]).ljust(24)
+                strhere += str(S0[i,j]).ljust(24)
+                strhere += str(S0_err[i,j]) + '\n'
+                f.write(strhere)
+
+def plot_triplet(tablename,plotname,SNR_cut=5.0,xmax=27.0,vmin_amp=None):
+
+    infile = './data/line_cubes/'+linehere+'.fits'
+    infile_freq = './data/line_cubes/'+linehere+'_freq.txt'
+    nu0 = nu0_dict[linehere]
+
+    hdul = fits.open(infile)
+    cube = hdul[0].data
+    nu = np.loadtxt(infile_freq)
+    nu /= (1.0e9)
+    v = ((nu0 - nu)/nu)*c
+
+    # get axes in arcseconds
+    head = hdul[0].header
+    pixnumx = np.linspace(0.0,head['NAXIS1']-1,head['NAXIS1'])
+    pixnumy = np.linspace(0.0,head['NAXIS2']-1,head['NAXIS2'])
+    x = head['CDELT1']*(pixnumx - head['CRPIX1']) + head['CRVAL1']
+    y = head['CDELT2']*(pixnumy - head['CRPIX2']) + head['CRVAL2']
+    x0 = head['CDELT1']*(np.ceil(cube.shape[1]/2) - head['CRPIX1']) + head['CRVAL1']
+    y0 = head['CDELT2']*(np.ceil(cube.shape[2]/2) - head['CRPIX2']) + head['CRVAL2']
+    x_arcsec = (x - x0)*3600.0
+    y_arcsec = (y - y0)*3600.0
+
+    i, j, A, A_err, s, s_err, v0, v0_err, S0, S0_err = np.loadtxt(tablename,unpack=True)
+    A = A.reshape(cube[0, :, :].shape)
+    A_err = A_err.reshape(cube[0, :, :].shape)
+    s = s.reshape(cube[0, :, :].shape)
+    s_err = s_err.reshape(cube[0, :, :].shape)
+    v0 = v0.reshape(cube[0, :, :].shape)
+    v0_err = v0_err.reshape(cube[0, :, :].shape)
+    S0 = S0.reshape(cube[0, :, :].shape)
+    S0_err = S0_err.reshape(cube[0, :, :].shape)
+
+    mask = (np.abs(A/A_err) < SNR_cut)
+    mask |= (np.abs(v0 - vrec) > 250.0)
+
+    A_plot = np.copy(A)
+    s_plot = np.copy(s)
+    v0_plot = np.copy(v0)
+    A_plot[mask] = np.nan
+    s_plot[mask] = np.nan
+    v0_plot[mask] = np.nan
+
+    xx,yy = np.meshgrid(x_arcsec,y_arcsec)
+
+    # initialize figure
+    fig = plt.figure(figsize=(9,3))
+    ax1 = fig.add_axes([0.05,0.05,0.3,0.9])
+    ax2 = fig.add_axes([0.375,0.05,0.3,0.9])
+    ax3 = fig.add_axes([0.70,0.05,0.3,0.9])
+    cax1 = fig.add_axes([0.05,0.96,0.3,0.03])
+    cax2 = fig.add_axes([0.375,0.96,0.3,0.03])
+    cax3 = fig.add_axes([0.70,0.96,0.3,0.03])
+
+    ax1.set_facecolor('black')
+
+    # plot peak line intensity
+    if vmin_amp is None:
+        vmin = 0.0
+        vmax = np.nanmax(A_plot[(np.abs(xx) < xmax) & (np.abs(yy) < xmax)])
+        cmap_amp = 'afmhot_us'
+    else:
+        vmin = vmin_amp
+        vmax = -vmin_amp
+        cmap_amp = 'seismic'
+    cf = ax1.contourf(x_arcsec,y_arcsec,A_plot,255,cmap=cmap_amp,vmin=vmin,vmax=vmax)
+    ax1.set_xlim(xmax,-xmax)
+    ax1.set_ylim(-xmax,xmax)
+    ax1.set_xlabel(r'$\Delta$RA (arcsec)')
+    ax1.set_ylabel(r'$\Delta$DEC (arcsec)')
+    ax1.grid(color='gray',linewidth=0.5,alpha=0.3,linestyle='--')
+
+    # colorbar
+    mappable = matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=vmin,vmax=vmax), cmap=cmap_amp)
+    plt.colorbar(mappable,cax=cax1,orientation='horizontal')
+    cax1.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+    ax1.set_title(r'Peak flux density (Jy beam$^{-1}$)',pad=35)
+
+    # plot line velocity
+    vmin = -200.0
+    vmax = 200.0
+    cf = ax2.contourf(x_arcsec,y_arcsec,v0_plot - vrec,255,cmap='seismic',vmin=vmin,vmax=vmax)
+    ax2.set_xlim(xmax,-xmax)
+    ax2.set_ylim(-xmax,xmax)
+    ax2.set_xticklabels([])
+    ax2.set_yticklabels([])
+    ax2.grid(color='gray',linewidth=0.5,alpha=0.3,linestyle='--')
+
+    # colorbar
+    mappable = matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=vmin+vrec,vmax=vmax+vrec), cmap='seismic')
+    plt.colorbar(mappable,cax=cax2,orientation='horizontal')
+    cax2.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+    ax2.set_title(r'Line velocity (km s$^{-1}$)',pad=35)
+
+    # plot line dispersion
+    vmin = 0.0
+    vmax = 100.0
+    cf = ax3.contourf(x_arcsec,y_arcsec,s_plot,255,cmap='Blues_us',vmin=vmin,vmax=vmax)
+    ax3.set_xlim(xmax,-xmax)
+    ax3.set_ylim(-xmax,xmax)
+    ax3.set_xticklabels([])
+    ax3.set_yticklabels([])
+    ax3.grid(color='gray',linewidth=0.5,alpha=0.3,linestyle='--')
+
+    # colorbar
+    mappable = matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=vmin,vmax=vmax), cmap='Blues_us')
+    plt.colorbar(mappable,cax=cax3,orientation='horizontal')
+    cax3.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+    ax3.set_title(r'Line dispersion (km s$^{-1}$)',pad=35)
+
+    plt.savefig(plotname,dpi=300,bbox_inches='tight')
+    plt.close()
+
+def make_plot(linehere,SNR_cut=5.0,xmax=27.0,vmin_amp=None):
+    plotname = './plots/Circinus_'+linehere+'.png'
+    tablename = './data/line_cubes/gaussian_fits_'+linehere+'.txt'
+    plot_triplet(tablename,plotname,SNR_cut=SNR_cut,xmax=xmax,vmin_amp=vmin_amp)
+
+##################################################
+# make line plots (png)
+
+for linehere in list(make_plots.keys()):
+    if write_tables[linehere]:
+        if linehere in ['OH_36.94GHz']:
+            bounds_amp = [-0.01,0.01]
+            write_table(linehere,rms_ind_start=rms_ind_start_dict[linehere],bounds_amp=bounds_amp)
+        else:
+            write_table(linehere,rms_ind_start=rms_ind_start_dict[linehere])
+    if make_plots[linehere]:
+        if linehere in ['OH_36.94GHz']:
+            make_plot(linehere,SNR_cut=SNR_cut,xmax=xmax_dict[linehere],vmin_amp=-0.01)
+        else:
+            make_plot(linehere,SNR_cut=SNR_cut,xmax=xmax_dict[linehere])
+
+##################################################
+
 
